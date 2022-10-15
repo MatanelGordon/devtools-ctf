38bb8f1b5e190ffbff3d5fb93d1b3e57dc879d00
Matanel Mac <mmgordon.matanel@gmail.com>
2022-10-16 01:50:07 +0300

    added level fixes

--- public/scripts/matmon-basic.js
+++ public/scripts/matmon-basic.js
@@ -1,74 +1,35 @@
-function _0x5829(_0x2d0da5, _0x279e77) {
-	const _0x572e56 = _0x572e();
-	return (
-		(_0x5829 = function (_0x58296d, _0x2be3e3) {
-			_0x58296d = _0x58296d - 0xd0;
-			let _0x5f32ac = _0x572e56[_0x58296d];
-			return _0x5f32ac;
-		}),
-		_0x5829(_0x2d0da5, _0x279e77)
-	);
-}
+const db = new PouchDB('flags');
+const counterEl = document.getElementById('counter');
+const O = JSON.parse(document.getElementById('__DATA__').textContent);
+const part1 = atob(decodeURIComponent(O['f']));
+const part2 = atob(decodeURIComponent(O['g']));
 
-const _0x53537d = _0x5829;
-(function (_0xc90902, _0x2879bc) {
-	const _0x4dbce3 = _0x5829,
-		_0x28cf69 = _0xc90902();
-	while (!![]) {
-		try {
-			const _0x28b5cd =
-				(parseInt(_0x4dbce3(0xe0)) / 0x1) *
-					(-parseInt(_0x4dbce3(0xdb)) / 0x2) +
-				(parseInt(_0x4dbce3(0xd3)) / 0x3) *
-					(-parseInt(_0x4dbce3(0xdf)) / 0x4) +
-				(-parseInt(_0x4dbce3(0xd1)) / 0x5) *
-					(parseInt(_0x4dbce3(0xd5)) / 0x6) +
-				parseInt(_0x4dbce3(0xd4)) / 0x7 +
-				(-parseInt(_0x4dbce3(0xd8)) / 0x8) *
-					(parseInt(_0x4dbce3(0xe1)) / 0x9) +
-				parseInt(_0x4dbce3(0xda)) / 0xa +
-				(-parseInt(_0x4dbce3(0xd0)) / 0xb) *
-					(-parseInt(_0x4dbce3(0xdc)) / 0xc);
-			if (_0x28b5cd === _0x2879bc) break;
-			else _0x28cf69['push'](_0x28cf69['shift']());
-		} catch (_0xbce7d) {
-			_0x28cf69['push'](_0x28cf69['shift']());
-		}
+void (async () => {
+	try {
+		const counterDoc = await db.get('counter');
+		const value = counterDoc.counter + 2;
+		await db.put({
+			...counterDoc,
+			counter: value,
+		});
+		counterEl.innerText = value.toString();
+	} catch (e) {
+		await db.bulkDocs([
+			{
+				flag_part1: part1,
+				_id: `_local/flag_part_1`,
+			},
+			{
+				flag_part2: part2,
+				_id: `flag_part_2`,
+			},
+			{
+				counter: 0,
+				_id: 'counter',
+			},
+		]);
+		counterEl.innerText = '0';
+	} finally {
+		counterEl.classList.add('floatIn');
 	}
-})(_0x572e, 0xa230c);
-const db = new PouchDB(_0x53537d(0xd7)),
-	O = JSON['parse'](document[_0x53537d(0xd9)]('__DATA__')[_0x53537d(0xd6)]),
-	part1 = atob(decodeURIComponent(O['f'])),
-	part2 = atob(decodeURIComponent(O['g']));
-db[_0x53537d(0xd2)]({ flag_part1: part1, _id: _0x53537d(0xde) }),
-	db[_0x53537d(0xd2)]({
-		flag_part2: part2,
-		_id: _0x53537d(0xdd),
-	});
-
-function _0x572e() {
-	const _0x3ffedf = [
-		'22HyjFoB',
-		'3681655wjPXvz',
-		'put',
-		'30rnYCTU',
-		'7239330YvCtoV',
-		'6SbwdID',
-		'textContent',
-		'flags',
-		'1448RwGoDQ',
-		'getElementById',
-		'3078660aMhEsu',
-		'560WoOQUY',
-		'20644980VsSMPr',
-		'flag_part_2',
-		'_local/flag_part_1',
-		'529124sQJCxC',
-		'3119KMAEib',
-		'58977VcAyOc',
-	];
-	_0x572e = function () {
-		return _0x3ffedf;
-	};
-	return _0x572e();
-}
+})();
